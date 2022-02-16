731ef382a63fbec4228f32f42c328a43e87c6810
diego ripari <diego_ripari@hotmail.com>
2022-02-16 16:26:33 -0300

    logre second commit sin la ayuda de alex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+def category
+  x = ["chinese", "italian", "japanese", "french", "belgian"]
+r = rand(0..4)
+return x[r]
+end
+5.times do
+  restaurants = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone, category: "#{category}" )
+  restaurants.save
+end
